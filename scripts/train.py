6f6c1a289a22daf8f6e75be710a428e661e4a600
Yakup Kaplan <66479180+yakupkaplan@users.noreply.github.com>
2020-12-23 14:04:21 +0100

    Add files via upload

---
+++ scripts/train.py
@@ -0,0 +1,102 @@
+# TRAIN SCRIPT FOR HOME CREDIT DEFAULT PREDICTION
+
+import gc
+import pandas as pd
+
+from lightgbm import LGBMClassifier
+import numpy as np
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import KFold, StratifiedKFold
+
+from scripts.helper_functions import display_importances
+
+# LightGBM GBDT with KFold or Stratified KFold
+# Parameters from Tilii kernel: https://www.kaggle.com/tilii7/olivier-lightgbm-parameters-by-bayesian-opt/code
+def kfold_lightgbm(df, num_folds=10, stratified=False, debug=False):
+    """
+    LightGBM GBDT with KFold or Stratified KFold.
+    Parameters from Tilii kernel: https://www.kaggle.com/tilii7/olivier-lightgbm-parameters-by-bayesian-opt/code
+    Separates train and test sets. Trains the model with tuned hyperparameters(found by Bayesian optimization) and
+    creates feature importance dataframe.
+
+    Returns a dataframe that shows hightest 40 feature importances.
+
+    :param df: dataframe
+        dataframe to be trained
+
+    :param num_folds: int, default=10
+        int that shows the number of splits for cross validation.
+
+    :param stratified: bool
+        boolean that indicates, if cross validation will be applied stratified or not.
+
+    :param debug: bool
+        boolean that indicates, if the model will be run debug mode or not.
+
+    :return: dataframe
+
+    """
+
+    # Divide in training/validation and test data
+    train_df = df[df['TARGET'].notnull()]
+    test_df = df[df['TARGET'].isnull()]
+    print("Starting LightGBM. Train shape: {}, test shape: {}".format(train_df.shape, test_df.shape))
+    del df
+    gc.collect()
+
+    # Cross validation model
+    if stratified:
+        folds = StratifiedKFold(n_splits=num_folds, shuffle=True, random_state=1001)
+    else:
+        folds = KFold(n_splits=num_folds, shuffle=True, random_state=1001)
+    # folds = KFold(n_splits=10, shuffle=True, random_state=1001)
+
+    # Create arrays and dataframes to store results
+    oof_preds = np.zeros(train_df.shape[0])  # predicted valid_y
+    sub_preds = np.zeros(test_df.shape[0])  # submission preds
+    feature_importance_df = pd.DataFrame()  # feature importance
+    feats = [f for f in train_df.columns if f not in ['TARGET', 'SK_ID_CURR', 'SK_ID_BUREAU', 'SK_ID_PREV', 'index']]
+
+    for n_fold, (train_idx, valid_idx) in enumerate(folds.split(train_df[feats], train_df['TARGET'])):
+        train_x, train_y = train_df[feats].iloc[train_idx], train_df['TARGET'].iloc[train_idx]
+        valid_x, valid_y = train_df[feats].iloc[valid_idx], train_df['TARGET'].iloc[valid_idx]
+
+        # LightGBM parameters found by Bayesian optimization
+        clf = LGBMClassifier(
+            nthread=4,
+            n_estimators=10000,
+            learning_rate=0.02,
+            num_leaves=34,
+            colsample_bytree=0.9497036,
+            subsample=0.8715623,
+            max_depth=8,
+            reg_alpha=0.041545473,
+            reg_lambda=0.0735294,
+            min_split_gain=0.0222415,
+            min_child_weight=39.3259775,
+            silent=-1,
+            verbose=-1, )
+
+        clf.fit(train_x, train_y, eval_set=[(train_x, train_y), (valid_x, valid_y)],
+                eval_metric='auc', verbose=200, early_stopping_rounds=200)
+
+        oof_preds[valid_idx] = clf.predict_proba(valid_x, num_iteration=clf.best_iteration_)[:, 1]  # predicted valid_y
+        sub_preds += clf.predict_proba(test_df[feats], num_iteration=clf.best_iteration_)[:, 1] / folds.n_splits  # submission preds.
+
+        fold_importance_df = pd.DataFrame()
+        fold_importance_df["feature"] = feats
+        fold_importance_df["importance"] = clf.feature_importances_
+        fold_importance_df["fold"] = n_fold + 1
+        feature_importance_df = pd.concat([feature_importance_df, fold_importance_df], axis=0)
+        print('Fold %2d AUC : %.6f' % (n_fold + 1, roc_auc_score(valid_y, oof_preds[valid_idx])))
+        del clf, train_x, train_y, valid_x, valid_y
+        gc.collect()
+
+    print('Full AUC score %.6f' % roc_auc_score(train_df['TARGET'], oof_preds))
+    # Write submission file and plot feature importance
+    if not debug:
+        test_df['TARGET'] = sub_preds
+        test_df[['SK_ID_CURR', 'TARGET']].to_csv("predictions/reference_submission.csv", index=False)
+    display_importances(feature_importance_df)
+
+    return feature_importance_df
